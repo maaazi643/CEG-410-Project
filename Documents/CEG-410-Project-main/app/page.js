98a378fca457c87bfb66f6a26c437549584d50f4
maaazi643 <adeyemimuaz101@gmail.com>
2025-02-03 20:45:44 +0100

    rearranged folder

--- Documents/CEG-410-Project-main/app/page.js
+++ Documents/CEG-410-Project-main/app/page.js
@@ -1,6 +1,5 @@
 'use client'
 import { useState } from "react";
-import "chart.js/auto";
 import { slopeDeflection } from "./beamAnalysis";
 import DynamicBeamVisualization from "./DynamicBeamVisualisation";
 
